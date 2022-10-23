bc9178332272055372f9954937c685326df56577
AbhishekBhovar-CG <abhishek-anil.bhovar@capgemini.com>
2022-10-23 21:55:43 +0530

    Revisit the modeling section Vid-#8

--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,69 @@
+from enum import auto
+from random import choices
+from statistics import mode
+from unicodedata import decimal
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2) # preferred more then FloatField because Float has rounding issues
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True) # auto_add_now will add the date and time when the field is updated/added first time
+
+
+class Customer(models.Model):
+    # setting the deafult Value
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    # setting up the choices
+    MEMBERSHIP_CHOICES = [
+        # ('value_stored_db', 'human_readable_name')
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    # following is a choice field
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default = MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_COMPLETE, 'Complete'),
+        (PAYMENT_FAILED, 'Failed'),
+    ]
+    # django will automatically populate this field
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_PENDING)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    # when we delete the customer the address will be deleted 
+    # one address will be related to one customer
+    # cutomer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    # incase we need one to many relation i.e. one customer can have multiple addresses
+    cutomer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_place=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
